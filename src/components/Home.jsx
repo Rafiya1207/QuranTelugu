1e55d080f0ba9b0166fd1dda3596aa2dd4833894
Rafiya1207 <rafiya1207@gmail.com>
2024-12-14 19:27:09 +0530

    display surah list - flex

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import { surahs } from '../Surahs/surahs.js';
 import SurahNavigator from './SurahNavigator';
 const Home = () => {
 	return (
-		<div className='page-font'>
+		<div className='page-font surah-list'>
 			{
 				surahs.map((surah) =>
 					<SurahNavigator surah={surah} key={surah.name}/>)
